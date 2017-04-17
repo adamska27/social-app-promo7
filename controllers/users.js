2e991c64959f09b07d079c4d32de7a076220f095
Adamska <adams.roy27@gmail.com>
2017-04-17 11:58:16 +0200

    changement config controllers/users.js utilisation pg.Pool à la place de pg.connect et ajout limite caractères dans user form

--- controllers/users.js
+++ controllers/users.js
@@ -1,59 +1,64 @@
 const pg = require('pg');
-const urlBD = 'postgres://postgres:1234@localhost:5432/promo7';
+const Pool = require('pg').Pool;
+
+// by default the pool will use the same environment variables
+// as psql, pg_dump, pg_restore etc:
+// https://www.postgresql.org/docs/9.5/static/libpq-envars.html
+
+// you can optionally supply other values
+const config = {
+  host: 'localhost',
+  user: 'postgres',
+  password: '1234',
+  database: 'promo7',
+};
+
+process.on('unhandledRejection', function(e) {
+  console.log(e.message, e.stack)
+})
+
+// create the pool somewhere globally so its lifetime
+// lasts for as long as your app is running
+const pool = new Pool(config);
+
+
+// const urlBD = 'postgres://postgres:1234@localhost:5432/promo7';
 
 const getAll = (req, res) => {
-
-  pg.connect(urlBD, (err, client, done) => {
-    if (err) throw err;
-
-    client.query('SELECT * FROM apprenants', (err, result) => {
+    pool.query('SELECT * FROM apprenants', (err, result) => {
       if (err) throw err;
-
-      done();
 
       res.render('usersPages/usersList', {
         apprenants: result.rows
       });
     });
-  });
 };
 
 const getOne = (req, res) => {
 
-  pg.connect(urlBD, (err, client, done) => {
-    if (err) throw err;
-
-    client.query('SELECT * FROM apprenants WHERE id = $1', [req.params.id], (err, result) => {
+    pool.query('SELECT * FROM apprenants WHERE id = $1', [req.params.id], (err, result) => {
       if (err) throw err;
-
-      done();
 
       res.render('usersPages/user', {
         apprenant: result.rows,
         id: req.params.id,
       });
     });
-  });
 };
 
 const getSpecificUser = (req, res) => {
-  const search = req.query.search.toLowerCase();
+  const search = req.query.search;
 
-  pg.connect(urlBD, (err, client, done) => {
-    if (err) throw err;
-
-    client.query('SELECT * FROM apprenants where firstname = $1', [search], (err, result) => {
+    pool.query('SELECT * FROM apprenants where firstname = $1', [search], (err, result) => {
       if (err) throw err;
-      done();
+
       if (result.rows.length > 0) {
         res.render('usersPages/searchResults',{ apprenants: result.rows});
       } else {
         res.render('usersPages/noSearchResults');
       }
     });
-  });
 };
-
 
 //A revoir probleme sur la route
 const getUserProjects = (req, res) => {
@@ -73,31 +78,23 @@ const getUserProjects = (req, res) => {
 };
 
 const postUser = (req, res) => {
-  pg.connect(urlBD, (err, client, done) => {
-    if (err) throw err;
 
-    client.query('INSERT INTO apprenants VALUES ($1, $2)', [req.body.firstname.toLowerCase(), req.body.lastname.toLowerCase()], (err, result) => {
+    pool.query('INSERT INTO apprenants VALUES ($1, $2)', [req.body.firstname.toLowerCase(), req.body.lastname.toLowerCase()], (err, result) => {
       if (err) throw err;
-      done();
+
       res.render('usersPages/added',
       {
         firstname: req.body.firstname,
         lastname: req.body.lastname,
       });
     });
-  });
 };
 
 const deleteUser = (req, res) => {
   const userId = Number(req.params.id);
 
-  pg.connect(urlBD, (err, client, done) => {
-    if(err) throw err;
-
-    client.query('DELETE FROM apprenants WHERE id = $1', [userId], (err, result) => {
+    pool.query('DELETE FROM apprenants WHERE id = $1', [userId], (err, result) => {
       if(err) throw err;
-
-      done();
 
       res.render('usersPages/deleted',
       {
@@ -105,19 +102,14 @@ const deleteUser = (req, res) => {
         id: userId
       });
     });
-  });
 };
 
 const putUser = (req, res) => {
   const userId = Number(req.params.id);
 
-  pg.connect(urlBD, (err, client, done) => {
-    if(err) throw err;
-    client.query('UPDATE apprenants SET firstname = $1, lastname = $2 WHERE id = $3', [req.body.firstname, req.body.lastname, userId], (err, result) => {
+    pool.query('UPDATE apprenants SET firstname = $1, lastname = $2 WHERE id = $3', [req.body.firstname, req.body.lastname, userId], (err, result) => {
       if(err) throw err;
 
-      done();
-      console.log(userId);
       res.render('usersPages/updated',
       {
         firstname: req.body.firsname,
@@ -125,7 +117,6 @@ const putUser = (req, res) => {
         id: userId
       });
     });
-  });
 };
 
 module.exports = {
